653fc4d9da387657a13d9b6c0cae4d9184ebe42a
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-03-14 15:04:39 +0000

    Update differs.js

--- barlaman-tracker/differs.js
+++ barlaman-tracker/differs.js
@@ -1,16 +1,35 @@
-import { sendUpdates } from '../utils.js';
+import { sendUpdates } from "../utils.js";
 
 // differ for the data
 const diffJarida = async (a, b) => {
-    const diff = { added: [] };
-    for (let i of b.data) {
-        // added
-        if (!a?.data?.some?.((item) => item.id === i.id)) {
-            diff.added.push(i);
-        }
+  const diff = { added: [] };
+  for (let i of b.data) {
+    // added
+    if (!a?.data?.some?.((item) => item.id === i.id)) {
+      diff.added.push(i);
     }
-    if (!diff.added.length) return;
-    await sendUpdates({ category: 'jarida', type: b.type, diff });
+  }
+  if (!diff.added.length) return;
+  await sendUpdates({ category: "jarida", type: b.type, diff });
+};
+const diffAdala = async (a, b) => {
+  if (a.length === b.length) return;
+  const diff = { added: [], removed: [] };
+  const aSet = new Set(a.map((item) => JSON.stringify(item)));
+  const bSet = new Set(b.map((item) => JSON.stringify(item)));
+
+  for (let item of b) {
+    if (!aSet.has(JSON.stringify(item))) {
+      diff.added.push(item);
+    }
+  }
+  for (let item of a) {
+    if (!bSet.has(JSON.stringify(item))) {
+      diff.removed.push(item);
+    }
+  }
+  await sendUpdates({ category: "adala", diff });
+  return;
 };
 
-export { diffJarida };
+export { diffJarida, diffAdala };
