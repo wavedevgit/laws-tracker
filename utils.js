efa5f1644f009630b453d6b0b8ab949ae438f3d3
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-03-14 15:04:47 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -11,30 +11,75 @@ export async function readFile(path) {
 
 let sendUpdates = async (data) => {
   const bot = new Telegraf(process.env.POSTER_TOKEN || "token");
-  let updates = "";
-  for (let update of data.diff.added) {
-    updates += `• Nouveau numéro \`${
-      update.num
-    }\` ajouté avec la date \`${new Date(
-      update.date
-    ).toLocaleDateString()}\`\n\n`;
-  }
-  const markup = Markup.inlineKeyboard(
-    data.diff.added.map((update) =>
-      Markup.button.url(
-        `📩 ${update.num}`,
-        update.url.startsWith("/")
-          ? "http://www.sgg.gov.ma" + update.url
-          : update.url
+  if (data.category === "jarida") {
+    let updates = "";
+    for (let update of data.diff.added) {
+      updates += `• Nouveau numéro \`${
+        update.num
+      }\` ajouté avec la date \`${new Date(
+        update.date
+      ).toLocaleDateString()}\`\n\n`;
+    }
+    const markup = Markup.inlineKeyboard(
+      data.diff.added.map((update) =>
+        Markup.button.url(
+          `📩 ${update.num}`,
+          update.url.startsWith("/")
+            ? "http://www.sgg.gov.ma" + update.url
+            : update.url
+        )
       )
-    )
-  );
-  await bot.telegram.sendMessage(
-    "@jaridarasmia",
-    `*📢 Jarida rasmia a été modifier*\n\n\**Type:**\`${data.type}\`\n${updates}`,
-    { parse_mode: "MarkdownV2" }
-  );
-  await bot.telegram.sendMessage("@jaridarasmia", "📩PDFS", markup);
+    );
+    await bot.telegram.sendMessage(
+      "@jaridarasmia",
+      `*📢 Jarida rasmia a été modifier*\n\n\**Type:**\`${data.type}\`\n${updates}`,
+      { parse_mode: "MarkdownV2" }
+    );
+    await bot.telegram.sendMessage("@jaridarasmia", "📩PDFS", markup);
+  }
+  if (data.category === "adala") {
+    let updates = "";
+    for (let update of data.diff.added) {
+      updates += `• Nouveau fichier ajouté avec la date \`${new Date(
+        update.fileMeta.gregorianDate
+      ).toLocaleDateString()}\` (${update.fileMeta.LawType?.name} ${
+        update.fileMeta.theme.name
+      } ${update.fileMeta?.lawNumber || ""})\n\n`;
+    }
+    for (let update of data.diff.added) {
+      updates += `• ❌ Fichier supprimé avec la date \`${new Date(
+        update.fileMeta.gregorianDate
+      ).toLocaleDateString()}\` (${update.fileMeta.LawType?.name} ${
+        update.fileMeta.theme.name
+      } ${update.fileMeta?.lawNumber || ""})\n\n`;
+    }
+    let i = 0;
+
+    const btns = [
+      data.diff.added.map((update) => {
+        i += 1;
+        return Markup.button.url(
+          `📩 ${i} (ajouté)`,
+          "https://adala.justice.gov.ma/api/" + update.path
+        );
+      }),
+      data.diff.removed.map((update) => {
+        i += 1;
+        return Markup.button.url(
+          `📩 ${i} (supprimé)`,
+          "https://adala.justice.gov.ma/api/" + update.path
+        );
+      }),
+    ];
+
+    const markup = Markup.inlineKeyboard(btns);
+    await bot.telegram.sendMessage(
+      "@jaridarasmia",
+      `*📢 Adala a été modifier*\n${updates}`,
+      { parse_mode: "MarkdownV2" }
+    );
+    await bot.telegram.sendMessage("@jaridarasmia", "📩PDFS", markup);
+  }
 };
 let onReady = () => null;
 const whenReady = (action) => {
